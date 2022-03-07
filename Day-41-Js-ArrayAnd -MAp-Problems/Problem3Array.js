3fbc6c4e0c95562294d79c6527fc63e2a76638cd
Somanath Havinal <somusp75@gmail.com>
2022-03-07 20:23:06 +0530

    prime factor number added problem 3

--- Day-41-Js-ArrayAnd -MAp-Problems/Problem3Array.js
+++ Day-41-Js-ArrayAnd -MAp-Problems/Problem3Array.js
@@ -1,25 +1,64 @@
-const pro = require("prompt-sync");
-const prompt = pro();
+//defining console read write module for taking input from the user
+io= require('console-read-write');
+//making the function and declaring it async
+//async makes the function execute when called and wait for user input statement
+//if async is not defined then await can not be defined and all user inputs will be taken together
+let primeFactorsArray= new Array();
+async function main()
+{
+    let checkForFactors= false;
+    console.log("Please enter the first no. to get prime factors");
+    //taking input for calculating prime factors
+    let n= await io.read();
+    //iterating over all values, which divides n
+    for(let i=2;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            //finding if the particular value is  prime no or not
+            //check is bool value
+           check= prime(i);
+           if(!check)
+           {
+               //prints prime factors
+               console.log("Prime Factors: "+i);
+               primeFactorsArray.push(i);
+               checkForFactors=true;
 
-let inputnum = prompt("Enter a number : ");
-let primesArray = new Array();
-
-let index = 0;
-for (let i = 2; i <= inputnum; i++) {
-//check inputnum is divisible - find factors - 2 to number
-  if (inputnum % i == 0) {
-    //Check divisible number d is prime or not
-    let isPrime = 1;
-    for (let d = 2; d <= i / 2; d++) {
-      if (i % d == 0) {
-        isPrime = 0;
-      }
+           }
+           else
+           {
+               continue;
+           }
+        }
     }
-    // If i is Prime number
-    if (isPrime == 1) {
-      primesArray[index] = i;
-      index++;
+    //if check for factors remain false, then there are no factors of numbers and number itself is prime
+    if(!checkForFactors)
+    {
+        console.log("No has no prime factors, and itself is prime");
     }
-  }
+    console.log("Prime Factors Array: "+primeFactorsArray);
 }
-console.log("Prime factors : "+primesArray);
+function prime(n)
+{
+    let check=false;
+    //iterating loop upto n/2
+    for(let i=2;i<=Math.ceil(n/2);i++)
+    {
+        //if n is divisible by any iterator, then n is not prime no
+        if(n%i==0)
+        {
+            return true;
+            //console.log(n +" is not a prime number");
+            break;
+        }
+    }
+    //if check is still false
+    //then n is prime number.   
+    if(!check)
+    {
+        return false;
+        //console.log(n+" is a prime number.");
+    }
+}
+main();
